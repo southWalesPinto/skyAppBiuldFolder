cd295b85eba0c2c75dbf1b0428921e5ac30465e6
Mulugeta <mullerlondon24@gmail.com>
2026-04-13 22:59:54 +0100

    Added Meeting model fields
    Added fields for title, date, time, platform, message, created_by, and created_at to Meeting model.

--- meetings/models.py
+++ meetings/models.py
@@ -1,16 +1,19 @@
-from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Meeting(models.Model):
-    
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    platform = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         db_table = "meeting"
 
-
-
-class MeetingParticipant(models.Model):
-    
-    class Meta:
-        db_table = "meeting_participant"
-
+    def __str__(self):
+        return self.title
