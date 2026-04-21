613b1016caaa6424a66d4b602fc947fea96568ac
Mulugeta <mullerlondon24@gmail.com>
2026-04-22 00:07:11 +0100

    Updated meetings urls

--- meetings/urls.py
+++ meetings/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-
 from . import views
 
 app_name = "meetings"
 
+urlpatterns = [
+    path('', views.meeting_list, name='meeting_list'),
+    path('create/', views.create_meeting, name='create_meeting'),
+    path('upcoming/', views.upcoming_meetings, name='upcoming_meetings'),
+    path('weekly/', views.weekly_meetings, name='weekly_meetings'),
+    path('monthly/', views.monthly_meetings, name='monthly_meetings'),
+]
 
