7918a66d6162da7a751c365d029025e97d04bf32
Mulugeta <mullerlondon24@gmail.com>
2026-04-26 17:24:48 +0100

    Add delete meeting URL pattern

--- meetings/urls.py
+++ meetings/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('upcoming/', views.upcoming_meetings, name='upcoming_meetings'),
     path('weekly/', views.weekly_meetings, name='weekly_meetings'),
     path('monthly/', views.monthly_meetings, name='monthly_meetings'),
+    path('delete/<int:id>/', views.delete_meeting, name='delete_meeting'),
 ]
 
