adccc3e3f0d8d851fc4aada5fdb49712fd259fe4
southWalesPinto <128372669+southWalesPinto@users.noreply.github.com>
2026-04-27 14:40:22 +0100

    update meetings/views.py to add schedule view and template

--- meetings/views.py
+++ meetings/views.py
@@ -1,41 +1,49 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Meeting
+from django.shortcuts import get_object_or_404, redirect, render
+
 from .forms import MeetingForm
+from .models import Meeting
 
 
 def meeting_list(request):
     meetings = Meeting.objects.all()
-    return render(request, 'meetings/meeting_list.html', {'meetings': meetings})
+    return render(
+        request,
+        "meetings/schedule.html",
+        {
+            "meetings": meetings,
+            "active_nav": "schedule",
+        },
+    )
 
 
 def create_meeting(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = MeetingForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('meetings:meeting_list')
+            return redirect("meetings:meeting_list")
     else:
         form = MeetingForm()
 
-    return render(request, 'meetings/create_meeting.html', {'form': form})
+    return render(request, "meetings/create_meeting.html", {"form": form})
 
 
 def upcoming_meetings(request):
     meetings = Meeting.objects.all()
-    return render(request, 'meetings/upcoming_meetings.html', {'meetings': meetings})
+    return render(request, "meetings/upcoming_meetings.html", {"meetings": meetings})
 
 
 def weekly_meetings(request):
     meetings = Meeting.objects.all()
-    return render(request, 'meetings/weekly_meetings.html', {'meetings': meetings})
+    return render(request, "meetings/weekly_meetings.html", {"meetings": meetings})
 
 
 def monthly_meetings(request):
     meetings = Meeting.objects.all()
-    return render(request, 'meetings/monthly_meetings.html', {'meetings': meetings})
+    return render(request, "meetings/monthly_meetings.html", {"meetings": meetings})
+
 
 def delete_meeting(request, id):
     meeting = get_object_or_404(Meeting, id=id)
     meeting.delete()
-    return redirect('meetings:meeting_list')
-
+    return redirect("meetings:meeting_list")
